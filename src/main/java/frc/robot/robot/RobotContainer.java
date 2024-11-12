51cae5e313db47732af79e5b95f8ff9e19ffc372
jtgarlits <jtgarlits@gmail.com>
2024-11-12 11:42:55 -0500

    removed code we will add in class

--- src/main/java/frc/robot/robot/RobotContainer.java
+++ src/main/java/frc/robot/robot/RobotContainer.java
@@ -1,33 +1,19 @@
 package frc.robot.robot;
 
 import edu.wpi.first.wpilibj2.command.Command;
-import frc.robot.subsystems.arm.Arm;
 import frc.robot.subsystems.drivebase.Swerve;
 import frc.robot.subsystems.drivebase.generated.TunerConstants;
-import frc.robot.subsystems.shooter.Shooter;
 
 public class RobotContainer {
 
     // Subsystems
     private final Swerve swerve;
-    private final Arm arm;
-    private final Shooter shooter;
-
-    // Auto and Teleop controllers
-    private final Auto auto;
-    private final Teleop teleop;
 
     public RobotContainer() {
         
         //Initialize Subsystems
         swerve = TunerConstants.DriveTrain;
-        arm = new Arm();
-        shooter = new Shooter();
 
-        // Initialize Auto and Teleop classes
-        auto = new Auto(this);
-        teleop = new Teleop(this);
-        teleop.configureBindings();
     }
 
     /**
@@ -36,7 +22,7 @@ public class RobotContainer {
      * @return Autonomous Command.
      */
     public Command getAutonomousCommand() {
-        return auto.getAutonomousCommand();
+        return null;
     }
 
     // Getters for subsystems
@@ -44,11 +30,4 @@ public class RobotContainer {
         return swerve;
     }
 
-    public Arm getArm() {
-        return arm;
-    }
-
-    public Shooter getShooter() {
-        return shooter;
-    }
 }
