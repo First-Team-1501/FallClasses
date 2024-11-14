9198fd15651b5fab35e18d484cbac8d959534c8c
Ross Cuttriss <rcuttriss@ross-cuttriss-tech.tech>
2024-11-14 13:05:54 -0500

    Made some changes to the ShooterConfig.java but need to finish it

--- src/main/java/frc/robot/subsystems/shooter/ShooterConfig.java
+++ src/main/java/frc/robot/subsystems/shooter/ShooterConfig.java
@@ -1,5 +1,15 @@
 package frc.robot.subsystems.shooter;
 
+import com.revrobotics.CANSparkBase.IdleMode;
+import com.revrobotics.CANSparkLowLevel.MotorType;
+
 public class ShooterConfig {
     
+  // **** RIGHT SHOOTER CONFIG **** //
+
+  // CAN bus ID
+  public static int sight_ID = 20;
+
+  // TYpe configs
+  public static
 }
