4463d66324ae1c76d17078516d8c51e2d7247a95
Ross Cuttriss <rcuttriss@ross-cuttriss-tech.tech>
2024-11-14 13:01:45 -0500

    Need to recreate the ShooterConfig.java

--- src/main/java/frc/robot/commands/shooter/ShooterSpeedCommand.java
+++ src/main/java/frc/robot/commands/shooter/ShooterSpeedCommand.java
@@ -2,19 +2,35 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot.commands.swerve.shooter;
+package frc.robot.commands.shooter;
 
 import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystem.shooter.Shooter;
 
 public class ShooterSpeedCommand extends Command {
+  
+  // Create the variables
+  private final Shooter shooter;
+  private final double speed;
+  private boolean stop;
+
   /** Creates a new ShooterSpeedCommand. */
-  public ShooterSpeedCommand() {
+  public ShooterSpeedCommand(Shooter shooter, double speed, boolean stop) {
+    this.shooter = shooter;
+    this.speed = speed;
+    this.stop = stop;
+
+    addRequirements(shooter);
+    
     // Use addRequirements() here to declare subsystem dependencies.
   }
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {}
+  public void initialize() 
+  {
+    shooter.set(speed);
+  }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
@@ -22,11 +38,17 @@ public class ShooterSpeedCommand extends Command {
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted) 
+  {
+    if(stop)
+    {
+      shooter.stop();
+    }
+  }
 
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return false;
+    return !stop;
   }
 }
